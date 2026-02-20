d0babbd9509a2633a81fe93b6c9621a139b01b2c
Noob596 <amicus282@gmail.com>
2026-02-20 22:25:50 +0900

    최대 힙.cc

--- 백준/Silver/11279. 최대 힙/최대 힙.cc
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -3,13 +3,15 @@
 #include <algorithm>
 
 using namespace std;
-
+/*
+priority queue 를 쓰지 말고 구현해보기
+*/
 
 void key_add(int inp, vector < int >& tree){
-    tree.push_back(inp);
+    tree.push_back(inp); // 트리 맨 끝에 값을 넣고
     int idx = tree.size() - 1;
-    while(idx > 1 && tree[idx / 2] < tree[idx]){
-        int swap_temp;
+    while(idx > 1 && tree[idx / 2] < tree[idx]){ // 인덱스가 1이 되면 끝내고, 또한 부모 노드보다 크면
+        int swap_temp; // 부모 노드와 서로값을바꾼다.
         swap_temp = tree[idx / 2];
         tree[idx / 2] = inp;
         tree[idx] = swap_temp;
@@ -19,7 +21,7 @@ void key_add(int inp, vector < int >& tree){
 }
 
 void key_delete(vector < int >& tree){
-    if(tree.size() == 1){
+    if(tree.size() == 1){ // 트리가 비어있으면(인덱스 0에 널값이 들어가있다)
         cout << 0 << "\n";
         return;
     }
@@ -31,32 +33,33 @@ void key_delete(vector < int >& tree){
 
         //shift down
         int idx = 1;
-        while(idx * 2 <= tree.size() - 1){
+        while(idx * 2 <= tree.size() - 1){ // 맨 마지막 값을 [1]로 가져온 후 자식과 비교하면서 값을 서로 바꿀거다
+
             // 자식 중 큰것 찾기
             int target;
             int swap_temp;
             if(idx * 2 + 1 <= tree.size() - 1){ // 자식이 2개 있다면
                 
-                if(tree[idx * 2] > tree[idx * 2 + 1]){
+                if(tree[idx * 2] > tree[idx * 2 + 1]){ // 왼쪽 자식이 오른쪽보다 크다면 왼쪽 자식이 target
                     swap_temp = tree[idx * 2];
                     target = idx * 2;
                 }
                 else{
-                    swap_temp = tree[idx * 2 + 1];
+                    swap_temp = tree[idx * 2 + 1]; // 오른쪽 자식이 target이 된다
                     target = idx * 2 + 1;
                 }
 
             }
-            else{
+            else{ // 자식이 1개라면 target의 인덱스는 idx * 2이다
                 swap_temp = tree[idx * 2];
                 target = idx * 2;
             }
 
-            if (tree[idx] >= tree[target]) {
+            if (tree[idx] >= tree[target]) { // 그래서 target 값이 idx보다 크면 loop 탈출하기
                 break;
             }
 
-            tree[target] = tree[idx];
+            tree[target] = tree[idx]; // 아니면 두개를 swap 한다.
             tree[idx] = swap_temp;
             idx = target;
 
