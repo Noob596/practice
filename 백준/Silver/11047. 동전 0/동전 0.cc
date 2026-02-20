22fda61ee1a5b362d4b0080d0ed129cc55dabaec
Noob596 <amicus282@gmail.com>
2026-02-20 21:16:34 +0900

    동전 0.cc

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -8,24 +8,7 @@ int main(){
     long long K;
     cin >> N >> K;
     int cnt = 0;
-    /*
-    for(int i = 0; i < N; i++){
-        long temp;
-        cin >> temp;
-        coins.push_back(temp);
-    }
-
-    for(int i = coins.size() - 1; i >= 0; i--){
-        while(coins[i] <= K){
-            K = K - coins[i];
-            cnt++;
-        }
-
-        if(K == 0){
-            break;
-        }
-    }
-    */
+   
 
     // priority queue를 써보자.
 
@@ -51,3 +34,4 @@ int main(){
 
     cout << cnt;
 }
+
